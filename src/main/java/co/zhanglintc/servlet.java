8581e9dd915c482ec4989233df63e26ba393b983
zhanglintc <zhanglintc623@gmail.com>
2024-01-11 11:27:49 +0800

    init

---
+++ src/main/java/co/zhanglintc/servlet.java
@@ -0,0 +1,18 @@
+package co.zhanglintc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // super.doGet(req, resp);
+        PrintWriter pw = resp.getWriter();
+        pw.println("haha");
+        pw.close();
+    }
+}
